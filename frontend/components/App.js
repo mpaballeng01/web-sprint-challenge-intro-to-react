100d48dccd59d71debfd9e238a09410b295ee372
Mpaballeng <pabiemmakena@gmail.com>
2024-09-27 23:34:58 -0400

    completed sprint 6

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -6,15 +6,36 @@ const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
-  // ❗ Create state to hold the data from the API
-  // ❗ Create effects to fetch the data and put it in state
+  const [people, setPeople] = useState([]);
+  
+  const getPlanetFromPerson = (person, planets) => {
+    const planet = planets.find(planet => planet.id === person.homeworld);
+  return planet.name;
+  }
+
+  const getPeople = async () => {
+    const {data: peopleData} = await axios.get(urlPeople)
+    const {data: planetData} = await axios.get(urlPlanets)
+    const data = peopleData.map(person => {
+      return {name: person.name, planet: getPlanetFromPerson(person, planetData)}
+  })
+
+  setPeople(data);
+  }
+  useEffect(() => {
+  getPeople();
+}, [])
+
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {people.map(person => <Character key={person.name} name={person.name} planet={person.planet}/>)}
+
     </div>
-  )
+  );
 }
 
 export default App
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Character() { // ❗ Add the props
+function Character({name, planet})  { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
+  const [isOpen, setIsOpen] = useState(false);
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    <div className='character-card' onClick={() => setIsOpen(!isOpen)}>
+      <h3 className='character-name'>{name}</h3>
+      {isOpen && (
+      <p>
+       Planet: <span className='character-planet'>{planet}</span> 
+      </p>
+      
+      )}
     </div>
-  )
+  );
 }
 
 export default Character
